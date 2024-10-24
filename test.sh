d6d1803c481dfa92ab19f406b85c9622173d5454
himabinduak <bindu.akula@veltris.com>
2024-10-24 10:38:13 +0000

    testing
    Signed-off-by: himabinduak <bindu.akula@veltris.com>

---
+++ test.sh
@@ -0,0 +1,19 @@
+{
+    "key1": "value1",
+    "nested_list": [
+        "v1",
+        "v2",
+        "v3",
+        "v4"
+    ],
+    "nested_list_dict": {
+        "k1": "v1",
+        "k2": "v2",
+        "k3": "v3",
+        "k4": {
+            "sub_key1": "sub_value1",
+            "sub_key2": "sub_value2"
+        }
+    }
+}
+
